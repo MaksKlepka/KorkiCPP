485d32deeefc6b99706ff8d3a9dfcae71fe76dd3
MaksKlepka <157881318+MaksKlepka@users.noreply.github.com>
2024-02-23 19:32:51 +0100

    Now I actually submitted them

---
+++ P/Zadania/R1/R1.2/zad2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"B";
+    cout<<"A";
+    cout<<"R";
+    cout<<"D";
+    cout<<"Z";
+    cout<<"O";
+    cout<<"";
+    cout<<"D";
+    cout<<"L";
+    cout<<"U";
+    cout<<"G";
+    cout<<"I";
+    cout<<"";
+    cout<<"N";
+    cout<<"A";
+    cout<<"P";
+    cout<<"I";
+    cout<<"S";
+}
