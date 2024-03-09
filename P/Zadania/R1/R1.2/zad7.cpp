19906db2cd8a9dfddb9c33803a71efbd18f4ed56
Maciej <mac300@users.noreply.github.com>
2024-03-09 10:56:38 +0100

    koniec spotkania

--- P/Zadania/R1/R1.2/zad7.cpp
+++ P/Zadania/R1/R1.2/zad7.cpp
@@ -5,6 +5,23 @@ int main()
 {
 int liczba;
 cout<<"Podaj liczbe: "; cin>>liczba;
-cout<<"Twoja liczba + 1 to: "<<liczba+1;
+cout<<"Twoja liczba + 1 to: "<<liczba+1<<endl;
+//operatory prefiksowe i postfiksowe
+//liczba=liczba+1;
+//liczba+=1;
+//liczba*=2;
+//++liczba;
+//liczba++;
+//--liczba;
+//liczba--;
+int x = 1;
+int y;
+cout<<"x = "<<++x<<endl;
+cout<<"x = "<<x++<<endl;
+cout<<"x = "<<--x<<endl;
+cout<<"x = "<<x--<<endl;
+cout<<"x = "<<x<<endl;
+y=x++;
+y=++x;
 return 0;
 }
--- P/Zadania/R1/R1.2/zad8.cpp
+++ P/Zadania/R1/R1.2/zad8.cpp
@@ -8,6 +8,15 @@ cout<<"Podaj 3 liczby calkowite"<<endl;
 cout<<"Pierwsza liczba = "; cin>>liczba1;
 cout<<"Druga liczba = "; cin>>liczba2;
 cout<<"Trzecia liczba = "; cin>>liczba3;
-cout<<"Srednia arymetryczna to: "<<(liczba1+liczba2+liczba3)/3;
+cout<<"Srednia arymetryczna to: "<<(double)(liczba1+liczba2+liczba3)/3<<endl;
+//uwaga
+//int x;
+//double y;
+//y=(double)x; - OK
+//x=(int)y; - ZLE!!!
+
+//typy w operatorach arytmetycznych
+cout<<"suma = "<<liczba1+liczba2+1.0*liczba3<<endl;
 return 0;
+
 }
