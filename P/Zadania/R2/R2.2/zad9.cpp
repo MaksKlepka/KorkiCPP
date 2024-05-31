39305c32e04462be39a13d1e37990bbe06cb4382
MaksKlepka <157881318+MaksKlepka@users.noreply.github.com>
2024-05-31 11:11:55 +0200

    Update zad9.cpp

--- P/Zadania/R2/R2.2/zad9.cpp
+++ P/Zadania/R2/R2.2/zad9.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int potengowanie(long int i, long int m){
-    int  wynik=i;
-    for(int i=1;i<=m-1;i++){
+float potengowanie(float i, float m){
+    float wynik=i, var=2;
+    cout<<"Poczatek pot: i="<<i<<endl;
+    do{
+        ++var;
         wynik=wynik*i;
-    }
+    }while(var==m);
+    cout<<"koniec pot: wynik="<<wynik<<endl;
     return wynik;
 }
 
-long int pierwiastkowanie(long int n, long int m){
-    long int wynik;
+float pierwiastkowanie(float n, float m){
+    float wynik;
     if(n<=-1){
         n*=-1;
     }
-    for(long int i=2;potengowanie(i,m)<=n;i++){
+    cout<<"Poczatek pier: "<<endl;
+    for(float i=2;potengowanie(i,m)<=n;i++){
         wynik=i;
     }
+    cout<<"koniec pier: wynik="<<wynik<<endl;
 return wynik;
 }
 
 int main(){
-    long int n, m;
+    float n, m;
     cout<<"Podaj liczbe n (liczba ktora pierwiastkujesz): ";
     cin>>n;
     cout<<"Podaj liczbe m (liczba ktora pierwiastkujesz przez): ";
