c6e1d2b10f3e6af6619ca368cf936a4a415d3835
MaksKlepka <157881318+MaksKlepka@users.noreply.github.com>
2024-06-21 20:55:31 +0200

    Zrobione zad13, podpunkt A

--- P/Zadania/R2/R2.2/zad13.cpp
+++ P/Zadania/R2/R2.2/zad13.cpp
@@ -1,35 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int rownanie(int n){
-int a=n, b=n;
 
+/* to jest podpunkt (A) */
+long int rownanieA(long int n){
+long int a=n, b=n;
 do
 {
-if(a*a+b*b==n){
-    cout<<a<<"^2+"<<b<<"^2";
-    a--;
-}
-else{
-    a--;
-}
-
-if(a*a+b*b==n){
-    cout<<a<<"^2+"<<b<<"^2";
+    do{
+    if(a*a+b*b==n){
+        cout<<a<<"^2 + "<<b<<"^2 = "<<n<<endl;
+        a--;
+    }
+    else{
+        a--;
+    }
+    }while(a!=0);
     b--;
-}
-else{
-    b--;
-}
+    a=n;
 }while(a&&b!=0);
 return 0;
 }
 
+/* to bedzie podpunkt (B) */
+long int rownanieB(long int n){
+
+return 0;
+}
+
 int main(){
-    int n;
+    long int n;
     cout<<"Program wypisuje wszystkie mozliwe rozklady liczby n na sumy dwoch kwadratow. Prosze podac liczbe n: ";
     cin>>n;
-    cout<<"Rownania to:"<<endl;
-    rownanie(n);
+    cout<<"Rownania, uzywajac sposobu (a) to:"<<endl;
+    rownanieA(n);
+    cout<<"Rownania, uzywajac sposobu (b) to:"<<endl;
+    rownanieB(n);
     return 0;
 }
