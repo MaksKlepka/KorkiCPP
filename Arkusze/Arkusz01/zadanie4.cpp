8b7ca9d14c73d882a8ec04f0a9ab2edd187f5a4a
MaksKlepka <157881318+MaksKlepka@users.noreply.github.com>
2024-10-12 12:51:25 +0200

    update

--- Arkusze/Arkusz01/zadanie4.cpp
+++ Arkusze/Arkusz01/zadanie4.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <math.h>
 using namespace std;
 
 void wczytaj(const char* sciezka,int liczby[],string slowa[])
@@ -26,7 +27,33 @@ void wczytaj(const char* sciezka,int liczby[],string slowa[])
 
 }
 
-int main(){
+bool czyPierwsza(int n){
+    int s = sqrt(n);
+    for(int i=2 ; i<s ; i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+void goldbach(int n){
+    if(n%2!=0){
+        return;     //sprawdza czy liczba jest parzysta
+    }
+    int num1=0, num2=0;
+    for(int i=3; i<=n ; i++){
+        int r=n-i;
+        if((czyPierwsza(i)&&czyPierwsza(r))&&(num2-num1<=r-i)){
+            num2=r;
+            num1=i;
+        }
+    }
+    cout<<n<<" "<<num1<<" "<<num2<<endl;
+}
+
+int main()
+{
     int liczby[100];
     string slowa[100];
     const char* sciezka = "./dane/przyklad.txt";
