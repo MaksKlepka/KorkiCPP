af5fc5d69be0d24f10bf4b00493139cbb4b4d410
MaksKlepka <157881318+MaksKlepka@users.noreply.github.com>
2024-10-19 13:19:16 +0200

    update

--- Arkusze/Arkusz01/zadanie4.cpp
+++ Arkusze/Arkusz01/zadanie4.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <sstream>
 
-//#include <math.h>
 using namespace std;
 
 void wczytaj(const char* sciezka,int liczby[],string slowa[])
@@ -15,9 +14,9 @@ void wczytaj(const char* sciezka,int liczby[],string slowa[])
         int i=0;
         while (!plik.eof())
         {
-                plik>>liczby[i];
-                plik>>slowa[i];
-                ++i;
+            plik>>liczby[i];
+            plik>>slowa[i];
+            ++i;
         }
         plik.close();
     }
@@ -26,11 +25,9 @@ void wczytaj(const char* sciezka,int liczby[],string slowa[])
         cout<<"!!!!!!!!"<<endl;
     }
 
-
 }
 
 bool czyPierwsza(int n){
-    //int s = sqrt(n);
     for(int i=2 ; i*i<n ; i++){
         if(n%i==0){
             return false;
@@ -40,6 +37,7 @@ bool czyPierwsza(int n){
 }
 
 string goldbach(int n){
+    stringstream wynik;
     if(n%2!=0){
         return "";     //sprawdza czy liczba jest parzysta
     }
@@ -52,22 +50,68 @@ string goldbach(int n){
         }
     }
     cout<<n<<" "<<num1<<" "<<num2<<endl;
-
-    //trzeba zapisać do pliku!!!
-
+    wynik<<n<<" "<<num1<<" "<<num2;
+    return wynik.str();
 }
+
+string znadz_najdluszy_ciag(string slowo){
+    stringstream wynik;
+    string longest="";
+    string current="";
+    for(int i=0;i<slowo.length();i++){
+        if(current.length()==0||current[0]==slowo[i]){ // jezeli aktualne slowo jest puste LUB iterowany znak ze slowa jest taki sam jak ten przechowany
+            current+=slowo[i];
+        }
+        else{
+            if(current.length()>longest.length()){ //sprawdz, czy aktualny ciag nie jest dluzszy niz ten zapisany
+                longest=current; //jesli tak to zapisuje
+            }
+            current="";
+            current+=slowo[i]; //zapisujemy aktualna lietke
+        }
+    }
+    if(current.length()>longest.length()){ //sprawdz, czy aktualny ciag nie jest dluzszy niz ten zapisany
+        longest=current;
+    }
+    if(longest.length()==0){ //w przypadku, gdy caly wyraz skladal sie z tej samej litery
+        longest=current;
+    }
+    cout<<longest<<" "<<longest.length()<<endl;
+    wynik<<longest<<" "<<longest.length()<<endl;
+    return wynik.str();
+}
+
+void zapisz(const char* sciezka,int liczby[]){
+    int i=0;
+    fstream plik;
+    plik.open(sciezka,ios_base::in|ios_base::out);
+            while(i!=100)
+            {
+                if(goldbach(liczby[i])!=""){
+                    plik<<goldbach(liczby[i])<<endl;
+                    ++i;
+                }
+                else{
+                    ++i;
+                }
+            }
+    plik.close();
+    return;
+}
+
+
 
 int main()
 {
     int liczby[100];
     string slowa[100];
-    const char* sciezka = "./dane/przyklad.txt";
-    
-    wczytaj(sciezka,liczby,slowa);
 
-    goldbach(20);
+    const char* sciezka1 = "./dane/pary.txt";   //potrzebny do wpisania danych
+    const char* sciezka2 = "./dane/wyniki4.txt";    //potrzebny do zapisu danych
 
-    
+    wczytaj(sciezka1,liczby,slowa);
+    zapisz(sciezka2,liczby);
+    znadz_najdluszy_ciag(slowa[1]);
 
     return 0;
 }
