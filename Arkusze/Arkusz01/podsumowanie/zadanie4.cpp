cadce484a13200d2a7b79656b820469a016f44cf
Maciej Niebrzydowski <mac300@users.noreply.github.com>
2024-12-14 15:03:40 +0100

    Arkusz 1 - podsumowanie

---
+++ Arkusze/Arkusz01/podsumowanie/zadanie4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void wczytaj(int liczba[], string slowa[], const char sciezka){
+    
+
+
+    return;
+}
+
+void liczba_goldbacha(){
+    return;
+}
+
+void zapisz(){
+    return;
+}
+
+int main(){
+    int liczba[100];
+    string slowa[100];
+    const char *sciezka = "/dane/przyklad.txt";
+
+}
