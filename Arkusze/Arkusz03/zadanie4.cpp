84bc16b400fe0cb7763deae300c1a10558f1f3b4
MaksKlepka <157881318+MaksKlepka@users.noreply.github.com>
2025-02-14 23:10:49 +0100

    Zadanie 4 zrobione

--- Arkusze/Arkusz03/zadanie4.cpp
+++ Arkusze/Arkusz03/zadanie4.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -20,6 +21,57 @@ void wczytaj(const char* sciezka, int liczby[]){
         cout<<"Plik sie nie wczytal!";
     }
     return;
+}
+
+
+bool czy_w_przedziale(int liczba){
+    if(liczba>=100&&liczba<=5000){
+        return true;
+    }
+    return false;
+}
+
+bool czy_jest_pierwsza(int liczba){
+    if(liczba==0||liczba==1){
+        return false;
+    }
+    for(int i=2;i<=liczba/2;++i){
+        if(liczba%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+string polaczenie_przedzialu_i_pierwszej(int liczby[]){
+    string wynik = "";
+    for(int i=0;i<300;++i){
+        if(czy_jest_pierwsza(liczby[i]) && czy_w_przedziale(liczby[i])){
+            wynik+=to_string(liczby[i])+"\n";
+        }
+    }
+    return wynik;
+}
+
+int obrocenie_liczby(int liczba){
+    int wynik=0, wspomagajaca;
+    while(liczba!=0){
+        wspomagajaca=liczba%10;
+        wynik=wynik*10+wspomagajaca;
+        liczba/=10;
+    }
+    return wynik;
+}
+
+string polaczenie_obrotu_i_pierwszej(int liczby[]){
+    string wynik="";
+    for(int i=0;i<200;++i){
+        int temp = obrocenie_liczby(liczby[i]);
+        if(czy_jest_pierwsza(temp)){
+            wynik+=to_string(liczby[i])+"\n";
+        }
+    }
+    return wynik;
 }
 
 int suma_cyfr_wystepujacych_w_liczbie(int N){
@@ -44,25 +96,25 @@ string ile_wag_ktore_rownaja_sie_1(int liczby_pierwsze[]){
     int ile=0;
     for(int i=0;i<200;i++){
         if(waga_liczb_do_liczba_jednocyfrowa(liczby_pierwsze[i])==1){
-            wynik+=liczby_pierwsze[i]+"\n"; //jak int zamienić na strung?
+            wynik+=to_string(liczby_pierwsze[i])+"\n";
             ++ile;
         }
     }
-    return ile+"\n"+wynik; //jak int zamienić na strung?
+    return "Illosc liczb z waga 1: "+to_string(ile)+"\n"+"\n"+wynik;
 }
 
-void wyniki4_3(const char* sciezka, int liczby_pierwsze[]){
+
+void wyniki(const char* sciezka, int liczby_pierwsze[],string wynik){
     fstream plik;
-    string wynik = ile_wag_ktore_rownaja_sie_1(liczby_pierwsze);
     plik.open(sciezka,ios_base::out);
     if(plik.is_open()){
-        plik<<"Zadanie 4.3"<<endl;
-        plik<<"Jest dokladnie "<<wynik<<" liczb ktorych waga rowna sie 1."<<endl<<endl;
+        plik<<wynik<<endl;
     }
     else{
         cout<<"Plik sie nie otworzyl przy zapisywaniu!";
     }
 }
+
 
 int main(){
     int liczby[300];
@@ -71,10 +123,14 @@ int main(){
     const char* sciezka1 = "./dane/liczby.txt";
     const char* sciezka2 = "./dane/pierwsze.txt";
 
-    const char* wyniki = "./odpowiedzi/wyniki4_3.txt";
+    const char* wyniki1 = "./odpowiedzi/wyniki4_1.txt";
+    const char* wyniki2 = "./odpowiedzi/wyniki4_2.txt";
+    const char* wyniki3 = "./odpowiedzi/wyniki4_3.txt";
 
     wczytaj(sciezka1, liczby);
     wczytaj(sciezka2, liczby_pierwsze);
 
-    wyniki4_3(wyniki,liczby_pierwsze);
+    wyniki(wyniki1,liczby,polaczenie_przedzialu_i_pierwszej(liczby));
+    wyniki(wyniki2,liczby_pierwsze,polaczenie_obrotu_i_pierwszej(liczby_pierwsze));
+    wyniki(wyniki3,liczby_pierwsze,ile_wag_ktore_rownaja_sie_1(liczby_pierwsze));
 }
