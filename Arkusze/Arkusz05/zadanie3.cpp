87eea3017cc398f8e3726c024409be4b204c8b26
MaksKlepka <157881318+MaksKlepka@users.noreply.github.com>
2025-04-26 12:38:21 +0200

    update

--- Arkusze/Arkusz05/zadanie3.cpp
+++ Arkusze/Arkusz05/zadanie3.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <set>
 
 using namespace std;
 
 int main(){
 
 }
+
+/*int main(){
+    int n,k;
+    string tekst, pomocnicza;
+    set <string> wzorce;
+
+    cin>>n;
+    cin>>tekst;
+
+    k=tekst.size();
+    for(int i=0;i<=k-n;i++){
+        pomocnicza="";
+        for(int j=0;j<n;j++){
+            pomocnicza+=tekst[i+j];
+        }
+        wzorce.insert(pomocnicza);
+    }
+    for(string i : wzorce){
+        cout<<i<<endl;
+    }
+    return 0;
+}*/
