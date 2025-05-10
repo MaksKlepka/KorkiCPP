df51d1561eb85df6f53031e5d2f91f55b03709cf
MaksKlepka <157881318+MaksKlepka@users.noreply.github.com>
2025-05-10 13:51:11 +0200

    Update

--- Arkusze/Arkusz05/zadanie3.cpp
+++ Arkusze/Arkusz05/zadanie3.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
 #include <set>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
-int main(){
-
+void wczytaj(const char* sciezka, int N[], string szyfrogram[]){
+    fstream plik;
+    plik.open(sciezka,ios_base::in);
+    if(plik.is_open())
+    {
+        int i=0;
+        while(!plik.eof())
+        {
+            plik>>N[i];
+            plik>>szyfrogram[i];
+            ++i;
+        }
+        plik.close();
+    }
+    else{
+        cout<<"Plik sie nie otworzyl!"<<endl;
+    }
+    return;
 }
 
-/*int main(){
-    int n,k;
-    string tekst, pomocnicza;
+void zadanie_3_1(int n, string tekst){
+    int k;
+    string pomocnicza;
     set <string> wzorce;
-
-    cin>>n;
-    cin>>tekst;
 
     k=tekst.size();
     for(int i=0;i<=k-n;i++){
@@ -26,5 +41,46 @@ int main(){
     for(string i : wzorce){
         cout<<i<<endl;
     }
-    return 0;
-}*/
+}
+
+int czy_zawiera(int N, string szyfrogram){
+    int k;
+    int count=0;
+    string pomocnicza;
+    set <string> wzorce;
+
+    k=szyfrogram.size();
+    for(int i=0;i<=k-N;i++){
+        pomocnicza="";
+        for(int j=0;j<N;j++){
+            pomocnicza+=szyfrogram[i+j];
+        }
+        for(int x=1;x<N;x++){
+            if(pomocnicza[0]!=pomocnicza[x]){
+                count++;
+                break;
+            }
+        }
+        pomocnicza.clear();
+    }
+    return count;
+}
+
+string zadanie_3_2(int N[], string szyfrogram[]){
+    stringstream wynik;
+    cout<<"Zadanie 3.2: "<<endl<<endl;
+    for(int i=0;i<10;i++){
+        wynik<<czy_zawiera(N[i],szyfrogram[i])<<endl;
+    }
+    return wynik.str();
+}
+
+int main(){
+    const char* sciezka_wczytaj = "./dane/szyfrogram.txt";
+    const char* sciezka_zapisz = "./odpowiedzi/wyniki3.txt";
+    int N[10];
+    string szyfrogram[10];
+
+    wczytaj(sciezka_wczytaj,N,szyfrogram);
+    cout<<zadanie_3_2(N,szyfrogram);
+}
